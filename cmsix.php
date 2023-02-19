3d42890835aa165ff3f2e65cfc5cec2b06b7697f
irek <irek@gabr.pl>
2023-02-19 16:48:09 +0100

    Use new line as separator, avoid types of content

--- cmsix.php
+++ cmsix.php
@@ -13,46 +13,18 @@ function read(?string $path = './db.txt'): array
     }
     while (($line = fgets($file))) {
         $parts = array_map('trim', explode("\t", $line));
-        $key   = $parts[0];
-        $type  = strtolower($parts[1]);
-        $entry;
-        // Search for non text content when there are more than 2
-        // parts in line.  Otherwise force default behavior.
-        switch (count($parts) > 2 ? $parts[1] : null) {
-        case 'area':            // Multi line textarea
-            $end = $parts[2];
-            $entry = '';
-            // TODO(irek): Validate all esential parts of line
-            // like this one that is very much required.
-            while (($line = fgets($file)) and trim($line) != $end) {
-                $entry .= $line;
-            }
-            unset($end);
-            $entry = trim($entry);
-            break;
-        case 'bool':
-            $entry = strtolower($parts[2]) == 'true';
-            break;
-        case 'int':
-            $entry = intval($parts[2]) ?? 0;
-            break;
-        case 'url':
-            $entry = [
-                'href'  => $parts[2] ?? '#',
-                'title' => $parts[3] ?? 'link',
-                'text'  => $parts[4] ?? 'link',
-            ];
-            break;
-        case 'img':
-            $entry = [
-                'src'   => $parts[2] ?? '',
-                'title' => $parts[3] ?? 'image',
-            ];
-            break;
-        default:              // Regular text, no type prefix required
-            $entry = $parts[1] ?? 'Lorem ipsum';
+        $key   = array_shift($parts);
+        $end   = array_shift($parts) ?? '';
+        $value = [];
+        while (($line = fgets($file)) and trim($line) != $end) {
+            array_push($value, trim($line));
         }
-        $data[$key] = $entry;
+        if ($end != '') {
+            $value = trim(implode("\n", $value));
+        } else if(count($value) == 1) {
+            $value = $value[0];
+        }
+        $data[$key] = $value;
     }
     fclose($file);
     ksort($data);
