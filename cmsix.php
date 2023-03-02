ee03fb885465f005837787c8ec33585f6b947aff
irek <irek@gabr.pl>
2023-03-02 17:35:04 +0100

    Use tabs instead of spaces for indentation

--- cmsix.php
+++ cmsix.php
@@ -12,66 +12,66 @@ const FPATH = './db.txt';
 /** Read data file from $path. */
 function read(?string $path = FPATH): array
 {
-    $lines = [];                // Stores value as trimed lines
-    $texts = [];                // Stores value original string
-    $file = fopen($path, 'rb');
-    if (!is_resource($file)) {
-        echo "Can't open '$path' file".PHP_EOL;
-        return $data;
-    }
-    while (($line = fgets($file))) {
-        if (strlen(trim($line)) == 0) {
-            continue;           // Skip empty lines
-        }
-        $parts = array_map('trim', explode("\t", $line));
-        $key = array_shift($parts);
-        $end = (array_shift($parts) ?? '')."\n";
-        $lines[$key] = [];
-        $texts[$key] = '';
-        while (($line = fgets($file)) and $line != $end) {
-            if (strlen(rtrim($line)) > 0) {
-                array_push($lines[$key], rtrim($line));
-            }
-            $texts[$key] .= $line;
-        }
-        $texts[$key] = rtrim($texts[$key]);
-    }
-    fclose($file);
-    ksort($lines);
-    ksort($texts);
-    return ['lines' => $lines, 'texts' => $texts];
+	$lines = [];            // Stores value as trimed lines
+	$texts = [];            // Stores value original string
+	$file = fopen($path, 'rb');
+	if (!is_resource($file)) {
+		echo "Can't open '$path' file".PHP_EOL;
+		return $data;
+	}
+	while (($line = fgets($file))) {
+		if (strlen(trim($line)) == 0) {
+			continue; // Skip empty lines
+		}
+		$parts = array_map('trim', explode("\t", $line));
+		$key = array_shift($parts);
+		$end = (array_shift($parts) ?? '')."\n";
+		$lines[$key] = [];
+		$texts[$key] = '';
+		while (($line = fgets($file)) and $line != $end) {
+			if (strlen(rtrim($line)) > 0) {
+				array_push($lines[$key], rtrim($line));
+			}
+			$texts[$key] .= $line;
+		}
+		$texts[$key] = rtrim($texts[$key]);
+	}
+	fclose($file);
+	ksort($lines);
+	ksort($texts);
+	return ['lines' => $lines, 'texts' => $texts];
 }
 
 /** Write $data to file $path. */
 function write(array $data, ?string $path = FPATH): bool {
-    $file = fopen($path, 'wb');
-    if (!is_resource($file)) {
-        echo "Can't open '$path' file".PHP_EOL;
-        return false;
-    }
-    foreach ($data['texts'] as $k => $v) {
-        fwrite($file, $k);
-        $v = str_replace('', '', $v);
-        $v = rtrim($v) . "\n";
-        // Append end indicator.  By default use single empty line but
-        // if $v value contains empty lines then generate unique id to
-        // mark end.
-        if (str_contains($v, "\n\n")) {
-            $id = "END-".uniqid();
-            fwrite($file, "\t".$id);
-            $v .= $id."\n";
-        }
-        fwrite($file, "\n".$v."\n");
-    }
-    return fclose($file);
+	$file = fopen($path, 'wb');
+	if (!is_resource($file)) {
+		echo "Can't open '$path' file".PHP_EOL;
+		return false;
+	}
+	foreach ($data['texts'] as $k => $v) {
+		fwrite($file, $k);
+		$v = str_replace('', '', $v);
+		$v = rtrim($v) . "\n";
+		// Append end indicator.  By default use single empty line but
+		// if $v value contains empty lines then generate unique id to
+		// mark end.
+		if (str_contains($v, "\n\n")) {
+			$id = "END-".uniqid();
+			fwrite($file, "\t".$id);
+			$v .= $id."\n";
+		}
+		fwrite($file, "\n".$v."\n");
+	}
+	return fclose($file);
 }
 
 /** Filter $arr array by keys using $pattern. */
 function get(array $arr, string $pattern): array
 {
-    return array_filter(
-        $arr,
-        fn($key) => preg_match($pattern, $key),
-        ARRAY_FILTER_USE_KEY
-    );
+	return array_filter(
+		$arr,
+		fn($key) => preg_match($pattern, $key),
+		ARRAY_FILTER_USE_KEY
+	);
 }
--- index.php
+++ index.php
@@ -19,18 +19,18 @@ define('SESSION_EXPIRY', time() + 60*60);
 
 /** Enum for each possible page. */
 enum Page {
-    case Hash;
-    case Login;
-    case Logout;
-    case Admin;
+	case Hash;
+	case Login;
+	case Logout;
+	case Admin;
 }
 
 function set_auth_cookie(?string $value = ''): bool
 {
-    return setcookie(SESSION_COOKIE, $value, [
-        'expires' => SESSION_EXPIRY,
-        'samesite' => 'Strict',
-    ]);
+	return setcookie(SESSION_COOKIE, $value, [
+		'expires' => SESSION_EXPIRY,
+		'samesite' => 'Strict',
+	]);
 }
 
 session_start();
@@ -42,57 +42,57 @@ $url_root = array_pop($tmp);
 unset($tmp);
 
 if (!isset($page) and preg_match('/^\/hash/', $url_args)) {
-    $page = Page::Hash;
-    if (isset($_GET['password'])) {
-        echo password_hash($_GET['password'], PASSWORD_DEFAULT);
-        exit(0);
-    }
+	$page = Page::Hash;
+	if (isset($_GET['password'])) {
+		echo password_hash($_GET['password'], PASSWORD_DEFAULT);
+		exit(0);
+	}
 }
 
 if (!isset($page) and preg_match('/^\/login/', $url_args)) {
-    $page = Page::Login;
-    if (isset($_POST['password'])) {
-        if (password_verify($_POST['password'], PASSWORD_HASH)) {
-            // Login
-            $session = uniqid();
-            $_SESSION[SESSION_KEY] = $session;
-            set_auth_cookie($session);
-            header('Location: '.$url_root);
-            exit;
-        } else {
-            $msg = 'Invalid password';
-        }
-    }
+	$page = Page::Login;
+	if (isset($_POST['password'])) {
+		if (password_verify($_POST['password'], PASSWORD_HASH)) {
+			// Login
+			$session = uniqid();
+			$_SESSION[SESSION_KEY] = $session;
+			set_auth_cookie($session);
+			header('Location: '.$url_root);
+			exit;
+		} else {
+			$msg = 'Invalid password';
+		}
+	}
 }
 
 if (!isset($page) and preg_match('/^\/logout/', $url_args)) {
-    $page = Page::Logout;
-    $_SESSION[SESSION_KEY] = null;
-    setcookie(SESSION_COOKIE, '', time() -1);
+	$page = Page::Logout;
+	$_SESSION[SESSION_KEY] = null;
+	setcookie(SESSION_COOKIE, '', time() -1);
 }
 
 // Default to main admin page.
 if (!isset($page)) {
-    $page = Page::Admin;
-    $session = $_SESSION[SESSION_KEY] ?? null;
-    $cookie = $_COOKIE[SESSION_COOKIE] ?? null;
-    // Redirect to login page if not authorized.
-    if (!$session or !$cookie or $session !== $cookie) {
-        header('Location: '.$url_root.'login');
-        exit;
-    }
-    // Refresh session cookie.
-    set_auth_cookie($cookie);
-    //
-    $data = cmsix\read(FPATH);
-    if (isset($_POST[cmsix\PREFIX.'_update'])) {
-        foreach ($_POST as $k => $v) {
-            if (isset($data['texts'][$k])) {
-                $data['texts'][$k] = $v;
-            }
-        }
-        cmsix\write($data, FPATH);
-    }
+	$page = Page::Admin;
+	$session = $_SESSION[SESSION_KEY] ?? null;
+	$cookie = $_COOKIE[SESSION_COOKIE] ?? null;
+	// Redirect to login page if not authorized.
+	if (!$session or !$cookie or $session !== $cookie) {
+		header('Location: '.$url_root.'login');
+		exit;
+	}
+	// Refresh session cookie.
+	set_auth_cookie($cookie);
+	//
+	$data = cmsix\read(FPATH);
+	if (isset($_POST[cmsix\PREFIX.'_update'])) {
+		foreach ($_POST as $k => $v) {
+			if (isset($data['texts'][$k])) {
+				$data['texts'][$k] = $v;
+			}
+		}
+		cmsix\write($data, FPATH);
+	}
 }
 
 ?>
@@ -110,33 +110,33 @@ if (!isset($page)) {
 <title>cmsix</title>
 <style>
 body {
-    background-color: #efefef;
-    padding: min(4vw, 4vh);
-    max-width: 42em;
-    margin: 0 auto;
-    line-height: 1.3;
+	background-color: #efefef;
+	padding: min(4vw, 4vh);
+	max-width: 42em;
+	margin: 0 auto;
+	line-height: 1.3;
 
-    /* source: http://web.mit.edu/jmorzins/www/ */
-    font-family:
-        "Bookman Old Style",    /* Windows, MacOS */
-        "Serifa BT",            /* Windows XP */
-        "URW Bookman L",        /* Unix+X+FontConfig */
-        "itc bookman",          /* Unix+X */
-        times, serif;           /* Fallback options */
+	/* source: http://web.mit.edu/jmorzins/www/ */
+	font-family:
+		"Bookman Old Style",    /* Windows, MacOS */
+		"Serifa BT",            /* Windows XP */
+		"URW Bookman L",        /* Unix+X+FontConfig */
+		"itc bookman",          /* Unix+X */
+		times, serif;           /* Fallback options */
 }
 a { color: #430cbd }
 a:visited { color: #066c16 }
 textarea {
-    min-width: 100%;
-    max-width: 100%;
+	min-width: 100%;
+	max-width: 100%;
 }
 footer {
-    text-align: right;
+	text-align: right;
 }
 @media (prefers-color-scheme: dark) {
-    body { background-color: #000; color: #fff }
-    a { color: #ffef6e }
-    a:visited { color: #6eff77 }
+	body { background-color: #000; color: #fff }
+	a { color: #ffef6e }
+	a:visited { color: #6eff77 }
 }
 </style>
 </head>
@@ -144,49 +144,49 @@ footer {
 <h1 hidden>cmsix</h1>
 
 <?php if ($page == Page::Hash): ?>
-    <h2>Hash</h2>
-    <p>Create password hash.</p>
-    <form>
-        <input type=password name=password placeholder=password required autofocus>
-        <input type=submit>
-    </form>
+	<h2>Hash</h2>
+	<p>Create password hash.</p>
+	<form>
+		<input type=password name=password placeholder=password required autofocus>
+		<input type=submit>
+	</form>
 <?php endif ?>
 
 <?php if ($page == Page::Login): ?>
-    <h2>Login</h2>
-    <p>Password should be given to you by website administrator.</p>
-    <?php if (isset($msg)) echo "<p><mark>$msg</mark></p>" ?>
-    <form method=post>
-        <input type=password name=password placeholder=password required autofocus>
-        <input type=submit value=login>
-    </form>
+	<h2>Login</h2>
+	<p>Password should be given to you by website administrator.</p>
+	<?php if (isset($msg)) echo "<p><mark>$msg</mark></p>" ?>
+	<form method=post>
+		<input type=password name=password placeholder=password required autofocus>
+		<input type=submit value=login>
+	</form>
 <?php endif ?>
 
 <?php if ($page == Page::Logout): ?>
-    <p>You have been logged out. Go back to:</p>
-    <ul>
-        <li><a href="/">home page</a></li>
-        <li><a href="<?=$url_root.'login'?>">login page</a></li>
-    </ul>
+	<p>You have been logged out. Go back to:</p>
+	<ul>
+		<li><a href="/">home page</a></li>
+		<li><a href="<?=$url_root.'login'?>">login page</a></li>
+	</ul>
 <?php endif ?>
 
 <?php if ($page == Page::Admin): ?>
-    <p><a href="<?=$url_root.'logout'?>">logout</a></p>
-    <h2>Admin page</h2>
-    <p>Modify at least one field and submit to make a change.</p>
-    <form method=post>
-        <?php foreach ($data['texts'] as $k => $v): ?>
-            <article id=<?=$k?>>
-                <h3><code><?=$k?></code></h3>
-                <textarea cols=80 rows=<?=substr_count($v, "\n")+1?> width=100% name=<?=$k?>><?=$v?></textarea>
-            </article>
-        <?php endforeach ?>
-        <p><input type=submit name=<?=cmsix\PREFIX?>_update></p>
-    </form>
+	<p><a href="<?=$url_root.'logout'?>">logout</a></p>
+	<h2>Admin page</h2>
+	<p>Modify at least one field and submit to make a change.</p>
+	<form method=post>
+		<?php foreach ($data['texts'] as $k => $v): ?>
+			<article id=<?=$k?>>
+				<h3><code><?=$k?></code></h3>
+				<textarea cols=80 rows=<?=substr_count($v, "\n")+1?> width=100% name=<?=$k?>><?=$v?></textarea>
+			</article>
+		<?php endforeach ?>
+		<p><input type=submit name=<?=cmsix\PREFIX?>_update></p>
+	</form>
 <?php endif ?>
 
 <footer>
-    <p><address id="contact"><a href="https://github.com/ir33k/cmsix">cmsix cms</a> v<?=cmsix\VERSION?></address></p>
+	<p><address id="contact"><a href="https://github.com/ir33k/cmsix">cmsix cms</a> v<?=cmsix\VERSION?></address></p>
 </footer>
 </body>
 </html>
